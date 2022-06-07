0906c35f5e7cea363946b4ecc719a248d5e92c86
Martin Heberling <martin.heberling.dalies@gmail.com>
2022-06-06 22:48:43 -0700

    Hid keys in separate python file and updated .gitignore.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import os
+import secrets
 
-myKey = os.environ["TRELLO_KEY"]
-myToken = os.environ["TRELLO_TOKEN"]
-url_member = "https://api.trello.com/1/members/me/boards?key=" + myKey + "&token=" + myToken # All member informatino
+myKey = secrets.TRELLO_KEY
+myToken = secrets.TRELLO_TOKEN
+url_member = "https://api.trello.com/1/members/me/boards?key=" + myKey + "&token=" + myToken # All member information
 url_boards = "https://api.trello.com/1/members/me/boards?fields=name,url&key=" + myKey + "&token=" + myToken # Get all board names
 
 def main():
